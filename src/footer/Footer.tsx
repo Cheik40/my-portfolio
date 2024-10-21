3f3090af84a6998cd47c4ca2583eef5af28a85d0
cheikh <djilyhoo@gmail.com>
2024-10-21 13:47:29 +0200

    import logo reseaux sociaux et redirection vers eux

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,12 +1,21 @@
+import githubIcon from "../assets/github.svg";
+import linkedinIcon from "../assets/linkedin.svg";
+
 function Footer() {
     return (
         <footer>
             <div className='home'>Revenir en haut</div>
             <div className='socialLink'>
-                <div className='email'></div>
-                <div className='tel'></div>
-                <div className='git'></div>
-                <div className='linkedin'></div>
+                <div className='git'>
+                    <a href='https://github.com/Cheik40'>
+                        <img src={githubIcon} alt='GitHub' />
+                    </a>
+                </div>
+                <div className='linkedin'>
+                    <a href='https://fr.linkedin.com/in/cheikh-ndiaye-870513196?trk=public_profile_browsemap_profile-result-card_result-card_full-click'>
+                        <img src={linkedinIcon} alt='LinkedIn' />
+                    </a>
+                </div>
             </div>
         </footer>
     );
