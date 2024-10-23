983dbbe1c26d21577851c3cd6aa8c3a56e48f3f7
cheikh <djilyhoo@gmail.com>
2024-10-23 12:37:07 +0200

    ajouter des logo competences et dans asset et import dans Skills.tsx

--- src/pages/skills/Skills.tsx
+++ src/pages/skills/Skills.tsx
@@ -1,6 +1,18 @@
 import styles from "./Skills.module.css";
 import mail from "../../assets/mail.svg";
-import { FaReact } from "react-icons/fa";
+import react from "../../assets/react.svg";
+import html from "../../assets/htlm.svg";
+import javascript from "../../assets/javascript.svg";
+import sass from "../../assets/sass.svg";
+import bootstrap from "../../assets/bootstrap.svg";
+import figma from "../../assets/figma.svg";
+import git from "../../assets/git.svg";
+import nodejs from "../../assets/nodejs.svg";
+import css from "../../assets/css.svg";
+import mongo from "../../assets/mongo.svg";
+import typescript from "../../assets/typescript.svg";
+import mysql from "../../assets/mysql.svg";
+// import css from "../../assets/css.svg";
 
 function Skills() {
     return (
@@ -12,60 +24,59 @@ function Skills() {
                         <h3 className={styles.label}>Using NOW:</h3>
                         <div className={styles.row}>
                             <div className={styles.col}>
-                                {/* <img src={mail} alt='mail' /> */}
-                                <FaReact />
-                                <h3>REACT</h3>
-                            </div>
-
-                            <div className={styles.col}>
-                                <img src={mail} alt='mail' />
-                                <h4>JAVASCRIPT</h4>
-                            </div>
-                            <div className={styles.col}>
-                                <img src={mail} alt='mail' />
+                                <img src={html} alt='logo HTML' />
                                 <h4>HTML</h4>
                             </div>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
+                                <img src={css} alt='logo css' />
                                 <h3>CSS3</h3>
+                            </div>
+                            <div className={styles.col}>
+                                <img src={react} alt='logo react' />
+
+                                <h3>REACT</h3>
+                            </div>
+                            <div className={styles.col}>
+                                <img src={javascript} alt='logo javascript' />
+                                <h4>JAVASCRIPT</h4>
                             </div>
                         </div>
 
                         <div className={styles.row}>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
+                                <img src={sass} alt='logo sass' />
                                 <h3>SASS</h3>
                             </div>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
+                                <img src={bootstrap} alt='logo bootstrap' />
                                 <h3>BOOTSTRAP</h3>
                             </div>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
+                                <img src={figma} alt='logo figma' />
                                 <h3>FIGMA</h3>
                             </div>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
+                                <img src={git} alt='logo git' />
                                 <h3>GIT</h3>
                             </div>
                         </div>
 
                         <div className={styles.row}>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
+                                <img src={nodejs} alt='logo nodejs' />
                                 <h3>NODEJS</h3>
                             </div>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
-                                <h3>BOOTSTRAP</h3>
+                                <img src={mongo} alt='logo mongobd' />
+                                <h3>MONGOBD</h3>
                             </div>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
-                                <h3>FIGMA</h3>
+                                <img src={typescript} alt='Logo typesvript' />
+                                <h3>TYPESCRIPT</h3>
                             </div>
                             <div className={styles.col}>
-                                <img src={mail} alt='mail' />
-                                <h3>GIT</h3>
+                                <img src={mysql} alt='logo mysql' />
+                                <h3>MYSQL</h3>
                             </div>
                         </div>
                     </div>
