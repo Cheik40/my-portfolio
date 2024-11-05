106061c83b9006c33ecc6077082375c05fab9674
cheikh <djilyhoo@gmail.com>
2024-11-05 13:07:58 +0100

    rendre responsive a-propos(about.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Headers from "./componants/headers/Headers";
 import About from "./pages/about/About";
 import Home from "./pages/home/Home";
-import Skills from "./pages/skills/Skills";
-import Experiances from "./pages/experiances/Experiances";
-import Contact from "./pages/contact/Contact";
-import Footer from "./componants/footer/Footer";
+// import Skills from "./pages/skills/Skills";
+// import Experiances from "./pages/experiances/Experiances";
+// import Contact from "./pages/contact/Contact";
+// import Footer from "./componants/footer/Footer";
 
 function App() {
     return (
         <>
             <Headers />
             <Home />
-            {/* <About /> */}
+            <About />
             {/* <Skills /> */}
             {/* <Experiances /> */}
             {/* <Contact /> */}
