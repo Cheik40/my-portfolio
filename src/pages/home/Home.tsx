74d05e4fe77aa609c8b9471e5d6a1a1ce02b7ef8
cheikh <djilyhoo@gmail.com>
2024-11-08 21:09:03 +0100

    correction rendre responsive home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,11 @@
 // import React from "react";
-import emailIcon from "../../assets/VectorAt.svg";
-import githubIcon from "../../assets/VectorGithub.svg";
-import linkedinIcon from "../../assets/Vector.png";
+import emailIcon from "../../assets/mail.svg";
+import githubIcon from "../../assets/github.svg";
+import linkedinIcon from "../../assets/linkedin.svg";
 import profilePhoto from "../../assets/djily.png";
+import email from "../../assets/VectorAt.svg";
+import github from "../../assets/VectorGithub.svg";
+import linkedin from "../../assets/Vector.png";
 import styles from "./Home.module.css";
 import "./Home.module.css";
 
@@ -12,7 +15,7 @@ function Home() {
             <div className={styles.introText}>
                 <h1 className={styles.titleH1}>Cheikh NDIAYE</h1>
                 <p className={styles.jobTitle}>Developer Front-end</p>
-                <div className={styles.socialIcons}>
+                <div className={`${styles.desktop} ${styles.socialIcons}`}>
                     <a
                         className={styles.socialLink}
                         href='mailto:djilyhoo@gmail.com'
@@ -30,6 +33,26 @@ function Home() {
                         href='https://fr.linkedin.com/in/cheikh-ndiaye-870513196?trk=public_profile_browsemap_profile-result-card_result-card_full-click'
                     >
                         <img src={linkedinIcon} alt='LinkedIn' />
+                    </a>
+                </div>
+                <div className={`${styles.socialIcons} ${styles.smallScreen}`}>
+                    <a
+                        className={styles.socialLink}
+                        href='mailto:djilyhoo@gmail.com'
+                    >
+                        <img src={email} alt='Email' />
+                    </a>
+                    <a
+                        className={styles.socialLink}
+                        href='https://github.com/Cheik40'
+                    >
+                        <img src={github} alt='GitHub' />
+                    </a>
+                    <a
+                        className={styles.socialLink}
+                        href='https://fr.linkedin.com/in/cheikh-ndiaye-870513196?trk=public_profile_browsemap_profile-result-card_result-card_full-click'
+                    >
+                        <img src={linkedin} alt='LinkedIn' />
                     </a>
                 </div>
             </div>
