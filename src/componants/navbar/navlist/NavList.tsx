8ce3af8d0a67fd776c6dcabd4a60664c704677bc
cheikh <djilyhoo@gmail.com>
2024-11-12 02:21:03 +0100

    correction rendre responsive home

--- src/componants/navbar/navlist/NavList.tsx
+++ src/componants/navbar/navlist/NavList.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./NavList.module.css";
+import styles from "./NavList.module.css";
 
 interface NavListProps {
     children: React.ReactNode;
 }
 
 function NavList({ children }: NavListProps) {
-    return <ul>{children}</ul>;
+    return <ul className={styles.ul}>{children}</ul>;
 }
 
 export default NavList;
